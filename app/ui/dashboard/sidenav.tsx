f661d89b2327308265c51a8f0d58c17a81c4fff1
Mi Gao <398938578@qq.com>
2025-11-11 16:43:32 +0800

    Chapter 14: Adding Authentication; todo: something wrong with authConfig

--- app/ui/dashboard/sidenav.tsx
+++ app/ui/dashboard/sidenav.tsx
@@ -1,6 +1,7 @@
 import AcmeLogo from '@/app/ui/acme-logo';
 import NavLinks from '@/app/ui/dashboard/nav-links';
 import { SkeletonNavLinks } from '@/app/ui/skeletons';
+import { signOut } from '@/auth';
 import { PowerIcon } from '@heroicons/react/24/outline';
 import Link from 'next/link';
 import { Suspense } from 'react';
@@ -21,7 +22,12 @@ export default function SideNav() {
           <NavLinks />
         </Suspense>
         <div className="hidden h-auto w-full grow rounded-md bg-gray-50 md:block"></div>
-        <form>
+        <form
+          action={async () => {
+            'use server';
+            await signOut({ redirectTo: '/' });
+          }}
+        >
           <button className="flex h-[48px] w-full grow items-center justify-center gap-2 rounded-md bg-gray-50 p-3 text-sm font-medium hover:bg-sky-100 hover:text-blue-600 md:flex-none md:justify-start md:p-2 md:px-3">
             <PowerIcon className="w-6" />
             <div className="hidden md:block">Sign Out</div>
